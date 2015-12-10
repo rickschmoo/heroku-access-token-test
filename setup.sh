56a9f915e38af3c44c452d8ccbf24555233e27dc
Richard Boardman <rboardman@salesforce.com>
2015-12-09 16:22:21 -0800

    tidy

--- setup.sh
+++ setup.sh
@@ -2,16 +2,6 @@
 set -e
 # env
 
-if [ X"$TRAVIS_SECURE_ENV_VARS" = X"true" ]; then
-npm login <<!
-${NPM_USERNAME}
-${NPM_PASSWORD}
-${NPM_EMAIL}
-!
-else
-  echo "TRAVIS: TRAVIS_SECURE_ENV_VARS env variable not set to true"
-fi
-
 DEPLOY_CLONE_URL=https://$GITHUB_USER:$DEPLOY_REPO_ACCESS_TOKEN@$DEPLOY_SCRIPTS_REPO
 
 echo "**** Cloning private repo test"
