ad6bbe2e13963448a131ce72892f8a2923f779da
Richard Boardman <rboardman@salesforce.com>
2015-12-09 16:01:09 -0800

    git clone will fail

--- setup.sh
+++ setup.sh
@@ -1,8 +1,17 @@
-env
-if [[$BUILD_ASSETS -ne ""]]; then
-  echo HELLO2
-  echo $BUILD_ASSETS;
-else
-  echo HELLO
+#!/bin/bash
+set -e
+# env
 
+if [ X"$TRAVIS_SECURE_ENV_VARS" = X"true" ]; then
+npm login <<!
+${NPM_USERNAME}
+${NPM_PASSWORD}
+${NPM_EMAIL}
+!
+else
+  echo "TRAVIS: TRAVIS_SECURE_ENV_VARS env variable not set to true"
 fi
+
+echo "**** Cloning private repo test"
+echo $DEPLOY_SCRIPTS_REPO
+git clone $DEPLOY_SCRIPTS_REPO scripts-internal
